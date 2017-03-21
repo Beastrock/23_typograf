264285fd021d8d39c65954acc176e46d5fa3e25e
Beastrock <beastrock@mail.ru>
2017-03-22 01:00:17 +0300

    Refactor, decompose and change buttons events code. *Refactor selectors *Decompose indents *Change key binding event

--- static/buttons_logic.js
+++ static/buttons_logic.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
   //function copies result to clipboard
-  $('#copy-text-btn').click(function() {
-  	var $temp = $('<input>');
-  	$('body').append($temp);
-  	$temp.val($('#typographed-text-area').text()).select();
-  	document.execCommand('copy');
-  	$temp.remove();
+  $("#js-copy-text-btn").click(function() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($("#js-typographed-text-area").text()).select();
+    document.execCommand("copy");
+    $temp.remove();
   });
   //function cleans origin text area
-  $('#clear-text-btn').click(function() {
-  	if ($('#origin-text-area').text() != '') {
-  	  $('#origin-text-area').html('');
-  	  $('#typographed-text-area').html('')
-  	}
+  $("#js-clear-text-btn").click(function() {
+    if ($("#js-origin-text-area").text() != "") {
+      $("#js-origin-text-area").html("");
+      $("#js-typographed-text-area").html("");
+    };
   });
-  	$('body').on('keydown', function(e) {
-  	  if (e.ctrlKey && e.keyCode == 13) {
-  	    $("#typography-text-btn").click();
-  	        return false;
-  	    }
-  	});
+  //function sends form data on Ctrl+ENTER
+  $("#js-origin-text-area").on("keydown", function(e) {
+    if (e.ctrlKey && e.keyCode == 13) {
+      $("#js-typography-text-btn").submit();
+      return false;
+    };
   });
+});
